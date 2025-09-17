4953400957f3050bc27a887f59cd5f1e04b1140f
Hanane Oubaha <Hananeouba03@gmail.com>
2025-09-17 22:16:47 +0100

    US5 :j'ai cree la classe mére operation avec les attributs et methodes necessaires

--- untitled/src/metier/Operation.java
+++ untitled/src/metier/Operation.java
@@ -1,4 +1,27 @@
 package metier;
 
-public class Operation {
+import java.time.LocalDate;
+import java.util.UUID;
+
+public abstract class Operation {
+    protected String numero;
+    protected LocalDate date ;
+    protected double montant ;
+
+    // constructeur
+    public Operation(double montant) {
+        this.numero = UUID.randomUUID().toString();
+        this.date = LocalDate.now();
+        this.montant = montant;
+    }
+    public void afficherDetails() {
+        System.out.println("Numéro : " + numero);
+        System.out.println("Date : " + date);
+        System.out.println("Montant : " + montant);
+    }
+
+    // Getter pour le montant
+    public double getMontant() {
+        return montant;
+    }
 }
