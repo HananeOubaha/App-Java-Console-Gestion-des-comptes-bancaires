9ebecd08b50097ec5ea4d1bf56c2e25a5ec70493
Hanane Oubaha <Hananeouba03@gmail.com>
2025-09-17 17:06:03 +0100

    US5 : j'ai fais la meme chose pour la classe fille CompteEpargne que la class fille compteCourant

--- untitled/src/metier/CompteEpargne.java
+++ untitled/src/metier/CompteEpargne.java
@@ -1,4 +1,39 @@
 package metier;
 
-public class CompteEpargne {
+public class CompteEpargne extends Compte {
+    private double tauxInteret;
+
+    public CompteEpargne(String code, double solde, double tauxInteret) {
+        super(code,solde);
+        this.tauxInteret = tauxInteret;
+    }
+    @Override
+    public void retirer(double montant) {
+        if (montant <= 0) {
+            System.out.println("Retrait innaceptable : solde insuffisant .");
+            return;
+        }
+        this.solde -= montant;
+        System.out.println("Retrait de " + montant + "effectué avec succés.Nouveau solde = " + this.solde);
+    }
+    @Override
+    public double calculerInteret() {
+        return this.solde * this.tauxInteret;
+    }
+
+    @Override
+    public void afficherDetails() {
+        System.out.println("=== Compte épargne ===");
+        System.out.println("code: " + code);
+        System.out.println("Solde: " + solde);
+        System.out.println("Taux d'intéret : "+ tauxInteret);
+    }
+
+    // getter et setter
+    public double getTauxInteret() {
+        return tauxInteret;
+    }
+    public void setTauxInteret(double tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
 }
