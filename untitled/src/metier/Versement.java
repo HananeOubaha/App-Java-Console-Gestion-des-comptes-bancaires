a89d910e5cb11d4acd811f2e1e97cb9093c94a37
Hanane Oubaha <Hananeouba03@gmail.com>
2025-09-18 16:24:31 +0100

    Us5: j'ai creer la classe fille  Versement de la classe mere  operation

---
+++ untitled/src/metier/Versement.java
@@ -0,0 +1,16 @@
+package metier ;
+public class Versement extends Operation {
+    private String source;
+
+    //constructeur
+    public Versement(double montant , String source) {
+        super(montant);
+        this.source = source;
+    }
+
+    @Override
+     public void afficherDetails() {
+        super.afficherDetails();
+        System.out.printLn("Source : " + source);
+    }
+}
